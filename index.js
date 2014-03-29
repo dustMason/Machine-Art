0766ee4c1ea1f3efc486292e269bf04ad90e1003
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2014-03-29 14:03:51 -0700

    very simple serial protocol for steppers

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-var HID = require("node-hid"),
-    _ = require("underscore"),
-    N64Controller = require("n64controller"),
-    SerialPort = require("serialport");
-
-HID.devices().forEach(function(device) {
-  if (device.vendorId === 121 && device.productId === 6)
-    controller = new N64Controller(device.path);
-});
+var SerialPort = require("serialport");
 
 var serialport = new SerialPort.SerialPort("/dev/tty.usbserial-A603G6VO", {
   baudrate: 9600,
-  parser: SerialPort.parsers.readline("\n")
+  parser: SerialPort.parsers.readline("\n"),
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false
 });
 
-var dpadData = "";
+var instructions = [
+  [0,0],
+  [300,100],
+  [300,200],
+  [0,300],
+  [15,5],
+  [0,0],
+  [0,0]
+];
+
+function sendCoordinates(x, y) {
+  [x,y].forEach(function(coord) {
+    var buf = new Buffer(2);
+    buf.writeUInt16LE(coord, 0);
+    console.log("Sending "+coord+" over serial...");
+    serialport.drain(function(){
+      serialport.write(buf);
+    });
+  });
+}
 
 serialport.on("open", function() {
-  console.log("serial port open");
-  var sendAnalogStickDataOverSerial = function() {
-    var delta = [1,1];
-    if (dpadData) {
-      if (dpadData.match(/N/)) delta[1] = 0;
-      else if (dpadData.match(/S/)) delta[1] = 2;
-      if (dpadData.match(/W/)) delta[0] = 0;
-      else if (dpadData.match(/E/)) delta[0] = 2;
-      // moveIt(delta);
-      console.log(delta[0]);
-      serialport.write(delta[0]);
+  console.log("Serial port open.");
+  serialport.on("data", function(data) {
+    console.log(data);
+    if (parseInt(data) === 6 && instructions.length > 0) {
+      console.log("Motors are ready.");
+      var coords = instructions.pop();
+      sendCoordinates(coords[0], coords[1]);
     }
-  };
-
-  controller.on("dpad", function(direction) {
-    dpadData = direction;
-    sendAnalogStickDataOverSerial();
   });
 });
 
--- src/drawing.ino
+++ src/drawing.ino
@@ -39,24 +39,43 @@ void setup() {
   // stepperRight.moveTo(100);
 }
 
-short incomingByte = 0;
+const int MOTORS_READY = 6;
+volatile boolean notifiedReady = false;
+int currentCoordinates[2];
+
+unsigned int readInt16() {
+  return Serial.read() + (Serial.read() << 8);
+}
+
+void readCoordinates() {
+  Serial.println("Reading coordinates...");
+  currentCoordinates[0] = readInt16();
+  currentCoordinates[1] = readInt16();
+  Serial.print("Read 2 16 bit ints from Serial: ");
+  Serial.print(currentCoordinates[0], BIN);
+  Serial.print(", ");
+  Serial.println(currentCoordinates[1], BIN);
+}
 
 void loop() {
   // if (stepperLeft.distanceToGo() == 0) stepperLeft.moveTo(-stepperLeft.currentPosition());
   // if (stepperRight.distanceToGo() == 0) stepperRight.moveTo(-stepperRight.currentPosition());
-
-  if (Serial.available() > 0) {
-    incomingByte = Serial.parseInt();
-    Serial.println(incomingByte, DEC);
-    if (incomingByte > 0) {
-      stepperLeft.move(0);
-      stepperLeft.moveTo(10000);
-    } else if (incomingByte < 0) {
-      stepperLeft.move(0);
-      stepperLeft.moveTo(-10000);
-    } 
+  if (stepperLeft.distanceToGo() == 0 && stepperRight.distanceToGo() == 0 && !notifiedReady) {
+    Serial.println(MOTORS_READY);
+    notifiedReady = true;
+  } else {
+    if (Serial.available() > 0 && notifiedReady) {
+      readCoordinates();
+      Serial.print("Moving motors to ");
+      Serial.print(currentCoordinates[0]);
+      Serial.print(", ");
+      Serial.println(currentCoordinates[1]);
+      // TODO adjust speed of command to ensure both motors arrive at the same time
+      stepperLeft.moveTo(currentCoordinates[0]);
+      stepperRight.moveTo(currentCoordinates[1]);
+      notifiedReady = false;
+    }
   }
-
   stepperLeft.run();
   stepperRight.run();
 }
