3f7ee234862e74b7171aa005e9f444e96ca8de67
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2014-03-30 16:18:06 -0700

    (broken) trying ArduinoSerialCommand lib

--- index.js
+++ index.js
@@ -1,44 +1,65 @@
-var SerialPort = require("serialport");
+var SerialPort = require("serialport"),
+    WebSocketServer = require('ws').Server,
+    http = require('http'),
+    express = require('express'),
+    app = express();
 
 var serialport = new SerialPort.SerialPort("/dev/tty.usbserial-A603G6VO", {
   baudrate: 9600,
-  parser: SerialPort.parsers.readline("\n"),
-  dataBits: 8,
-  parity: 'none',
-  stopBits: 1,
-  flowControl: false
+  parser: SerialPort.parsers.readline("\n")
+  // dataBits: 8,
+  // parity: 'none',
+  // stopBits: 1,
+  // flowControl: false
 });
 
+var MOTORS_READY = 6;
+
 var instructions = [
-  [0,0],
-  [300,100],
-  [300,200],
-  [0,300],
-  [15,5],
-  [0,0],
   [0,0]
 ];
+var motorsAreBusy = true;
 
-function sendCoordinates(x, y) {
-  [x,y].forEach(function(coord) {
-    var buf = new Buffer(2);
-    buf.writeUInt16LE(coord, 0);
-    console.log("Sending "+coord+" over serial...");
-    serialport.drain(function(){
-      serialport.write(buf);
-    });
+app.use(express.static(__dirname + '/public'));
+
+var server = http.createServer(app);
+server.listen(8080);
+
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+  console.log('Opened WebSocketServer');
+  ws.on("message", function(data, flags) {
+    console.log("Got data over ws: ", data.split(","));
+    var coordStrings = data.split(",");
+    instructions.unshift([parseInt(coordStrings[0]), parseInt(coordStrings[1])]);
+    sendNextCoordinates();
   });
+  ws.on('close', function() {
+    console.log('Closing WebSocketServer');
+  });
+});
+
+function sendNextCoordinates(x, y) {
+  if (!motorsAreBusy && instructions.length > 0) {
+    serialport.drain(function(){
+      var command = "D" + instructions.pop().join(" ");
+      console.log("Sending:", command);
+      serialport.write(command + "\n");
+    });
+    motorsAreBusy = true;
+  }
 }
 
 serialport.on("open", function() {
   console.log("Serial port open.");
+  serialport.write("\nHELLO YOU\n");
   serialport.on("data", function(data) {
     console.log(data);
-    if (parseInt(data) === 6 && instructions.length > 0) {
+    if (parseInt(data) === MOTORS_READY) {
+      motorsAreBusy = false;
       console.log("Motors are ready.");
-      var coords = instructions.pop();
-      sendCoordinates(coords[0], coords[1]);
     }
+    sendNextCoordinates();
   });
 });
 
--- src/drawing.ino
+++ src/drawing.ino
@@ -2,11 +2,12 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
+#include <SerialCommand.h>
 
+SerialCommand SCmd;
 Adafruit_MotorShield AFMS(0x60);
 
 // Connect two steppers with 200 steps per revolution (1.8 degree)
-// to the top shield
 Adafruit_StepperMotor *lMotor = AFMS.getStepper(200, 1);
 Adafruit_StepperMotor *rMotor = AFMS.getStepper(200, 2);
 
@@ -28,9 +29,33 @@ void backwardStepRight() {
 AccelStepper stepperLeft(forwardStepLeft, backwardStepLeft);
 AccelStepper stepperRight(forwardStepRight, backwardStepRight);
 
+
+const int MOTORS_READY = 6;
+boolean notifiedReady = false;
+/* int currentCoordinates[2]; */
+/* int serialDelimeter = 0x0A; */
+
+void processDrawCommand() {
+  int aNumber;  
+  char *arg; 
+  arg = SCmd.next(); 
+  if (arg != NULL) {
+    aNumber = atoi(arg);
+    stepperLeft.moveTo(aNumber);
+    notifiedReady = false;
+  } 
+  arg = SCmd.next(); 
+  if (arg != NULL) {
+    aNumber = atoi(arg); 
+    stepperRight.moveTo(aNumber);
+    notifiedReady = false;
+  }
+}
+
 void setup() {
   AFMS.begin();
   Serial.begin(9600);
+  SCmd.addCommand("D", processDrawCommand);
   stepperLeft.setMaxSpeed(400.0);
   stepperLeft.setAcceleration(180.0);
   // stepperLeft.moveTo(100);
@@ -39,43 +64,14 @@ void setup() {
   // stepperRight.moveTo(100);
 }
 
-const int MOTORS_READY = 6;
-volatile boolean notifiedReady = false;
-int currentCoordinates[2];
-
-unsigned int readInt16() {
-  return Serial.read() + (Serial.read() << 8);
-}
-
-void readCoordinates() {
-  Serial.println("Reading coordinates...");
-  currentCoordinates[0] = readInt16();
-  currentCoordinates[1] = readInt16();
-  Serial.print("Read 2 16 bit ints from Serial: ");
-  Serial.print(currentCoordinates[0], BIN);
-  Serial.print(", ");
-  Serial.println(currentCoordinates[1], BIN);
-}
-
 void loop() {
   // if (stepperLeft.distanceToGo() == 0) stepperLeft.moveTo(-stepperLeft.currentPosition());
   // if (stepperRight.distanceToGo() == 0) stepperRight.moveTo(-stepperRight.currentPosition());
+  stepperLeft.run();
+  stepperRight.run();
   if (stepperLeft.distanceToGo() == 0 && stepperRight.distanceToGo() == 0 && !notifiedReady) {
     Serial.println(MOTORS_READY);
     notifiedReady = true;
-  } else {
-    if (Serial.available() > 0 && notifiedReady) {
-      readCoordinates();
-      Serial.print("Moving motors to ");
-      Serial.print(currentCoordinates[0]);
-      Serial.print(", ");
-      Serial.println(currentCoordinates[1]);
-      // TODO adjust speed of command to ensure both motors arrive at the same time
-      stepperLeft.moveTo(currentCoordinates[0]);
-      stepperRight.moveTo(currentCoordinates[1]);
-      notifiedReady = false;
-    }
   }
-  stepperLeft.run();
-  stepperRight.run();
+  SCmd.readSerial();
 }
